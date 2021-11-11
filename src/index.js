828025783619b94077501ccd28c61515d00d4b67
Lua <75901276+luana-fs@users.noreply.github.com>
2021-11-11 00:00:28 -0300

    endpoint createUser and getUserTasks

--- src/index.js
+++ src/index.js
@@ -1,25 +1,52 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const res = require('express/lib/response');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
-
+const users = [];
+console.log(users)
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const user = users.find((user) => user.username === username)
+
+  if(!user) {
+    response.status(404).send({message: "Usuário não existe."})
+  }
+
+  request.user = user
+  next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body
+
+  const id = uuidv4()
+
+  const newUser = {
+    id,
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(newUser)
+
+  // res.status(201).send(newUser)
+  response.status(201).send({newUser})
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+// const {username} = request.headers
+const {user} = request
+// const result = users.find((user) => user.username === username)
+
+response.status(200).send({tarefas: user.todos})
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
@@ -39,3 +66,5 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 module.exports = app;
+
+// app.listen(3333, () => console.log("Deu certo"))
